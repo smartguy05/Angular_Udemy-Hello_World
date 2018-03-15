3d8771da96b9a757168df196053d28c1f71e8777
smartguy05 <smartguy05@gmail.com>
2018-03-15 11:56:11 -0600

    adding dynamic content

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,21 @@ import { Component} from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    //template: '<h2>{{ "Title:" +  title }}</h2>'
+    template: `
+    <h2>{{ getTitle() }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{ course }}
+        </li>
+    </ul>
+    `
 })
 export class CoursesComponent {
+    title = "List of courses";
+    courses = ["course1", "course2", "course3"];
 
+    getTitle() {
+        return this.title;
+    }
 }
