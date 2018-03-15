fec16cf025cd0899adaffa89613d4a3fa838b317
smartguy05 <smartguy05@gmail.com>
2018-03-15 14:15:30 -0600

    custom pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,17 +7,27 @@ import { CoursesService } from './courses.service';
     templateUrl: './courses.component.html'
 })
 export class CoursesComponent {
-    title = "List of courses";
-    email = "test@test.com";
+    constructor() {}
+
+    text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at enim maximus eros sollicitudin accumsan. Proin vulputate quis tortor id rutrum. Aenean mollis et odio id viverra. Curabitur vitae neque vitae erat faucibus scelerisque et eu turpis. Pellentesque lacus ligula, faucibus at magna vel, mattis semper orci. Praesent nec arcu eu nibh placerat ultrices vitae ut urna. Donec porttitor sagittis malesuada. `;
+    // title = "List of courses";
+    //  course = {
+    //      title: "The Complete Angular Course",
+    //      rating: 4.9745,
+    //      students: 30123,
+    //      price: 190.95,
+    //      releaseDate: new Date(2016,3,1)
+    //  };
+    //email = "test@test.com";
     //courses;
     //imageUrl = "http://lorempixel.com/400/200";
     //colSpan = 2;
     //isActive = false;
     
-    onKeyUp(email) {
-        console.log(this.email);
-    }
-    constructor() {}
+    // onKeyUp(email) {
+    //     console.log(this.email);
+    // }
+    
     
     // constructor(service: CoursesService) {
     //     //this.courses = service.getCourses();
