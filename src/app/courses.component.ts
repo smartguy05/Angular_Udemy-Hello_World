4f81fa2e14f4a227a0e58fdd18ef824eb8b7849b
smartguy05 <smartguy05@gmail.com>
2018-03-15 13:22:54 -0600

    added bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,17 +5,27 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     //template: '<h2>{{ "Title:" +  title }}</h2>'
     template: `
-    <h2>{{ getTitle() }}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-    </ul>
+        <!--<table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>-->
+
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        <!--<img src = "{{ imageUrl }}" />
+        <img [src]="title" />-->
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    imageUrl = "http://lorempixel.com/400/200";
+    colSpan = 2;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
